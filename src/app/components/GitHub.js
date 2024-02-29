5122a3d45a484aa05a05a5f666e96bd734acdcbe
Mark J Rawlins <96557207+5F8575tree@users.noreply.github.com>
2024-02-29 15:28:42 +0900

    add linked-in and github links

--- src/app/components/GitHub.js
+++ src/app/components/GitHub.js
@@ -3,7 +3,9 @@ import styles from "../styles/gitHub.module.css";
 const GitHub = () => {
   return (
     <div className={styles.gitHub}>
-      <img className={styles.gitHub__image} src="/gitHub.png" />
+      <a href="https://github.com/5F8575tree">
+        <img className={styles.gitHub__image} src="/gitHub.png" />
+      </a>
     </div>
   );
 };
--- src/app/components/LinkedIn.js
+++ src/app/components/LinkedIn.js
@@ -3,7 +3,9 @@ import styles from "../styles/linkedIn.module.css";
 const LinkedIn = () => {
   return (
     <div className={styles.linkedIn}>
-      <img className={styles.linkedIn__image} src="/linkedIn.png" />
+      <a href="https://www.linkedin.com/in/markjrawlins/">
+        <img className={styles.linkedIn__image} src="/linkedIn.png" />
+      </a>
     </div>
   );
 };
