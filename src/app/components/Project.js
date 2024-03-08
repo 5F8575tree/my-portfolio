73e983a23f0988aa79f8d64002fdd5460815ff22
Mark J Rawlins <96557207+5F8575tree@users.noreply.github.com>
2024-03-08 11:38:03 +0900

    styling to make a little more interesting - font and shadows

--- src/app/components/Project.js
+++ src/app/components/Project.js
@@ -9,12 +9,11 @@ const Project = ({ title, intro, link, number }) => {
     <div className={classForGrid(number)}>
       <div className={styles.content}>
         <div className={styles.project__headers}>
-          <h2 className={styles.project__header}>project:</h2>
           <h3 className={styles.project__title}>{title}</h3>
         </div>
         <p className={styles.project__intro}>{intro}</p>
         <a href={link} target="_blank" className={styles.project__link}>
-          <p className={styles.project__link_text}>link to project</p>
+          <p className={styles.project__link_text}>View</p>
           {/* <svg className={styles.project__link_svg}></svg> */}
         </a>
       </div>
